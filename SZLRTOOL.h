06078ef022819eec25b80a3ca8fa60b00c7153f1
shizhilvren—VSCode <shizhilvren@foxmail.com>
2021-01-28 19:55:58 +0800

    submod测试

---
+++ SZLRTOOL.h
@@ -0,0 +1,9 @@
+#ifndef __SZLRTOOL_
+#define __SZLRTOOL_
+#include "./src/Tool/clock.h"
+#include "./src/Tool/tool.h"
+#include "./src/Tool/tool_array.h"
+
+#include "./src/Debuger/debug.h"
+#include "./src/Debuger/output.h"
+#endif /*__SZLRTOOL_*/
